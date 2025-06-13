0b03f178c881afdc10b7ac39dfcc4bf2ef477945
carl0s-dev <carlosmaletti998@gmail.com>
2025-06-13 16:20:34 -0300

    chore: setup database and user model

---
+++ bookshelf-api/src/infra/database/index.ts
@@ -0,0 +1,11 @@
+import { Sequelize } from 'sequelize'
+import { config } from '../config'
+
+export const sequelize = new Sequelize(config.DATABASE_URL)
+
+try {
+  await sequelize.authenticate()
+  console.log('Connection has been established successfully.')
+} catch (error) {
+  console.error('Unable to connect to the database:', error)
+}
---
+++ bookshelf-api/src/infra/models/user.model.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database'
+
+export const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+  }
+)
