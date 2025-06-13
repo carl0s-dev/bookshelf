c15fd89a09d270ca10a9d57700b26f9ace2b4f1a
carl0s-dev <carlosmaletti998@gmail.com>
2025-06-13 14:45:51 -0300

    feat: setup express with swagger

---
+++ bookshelf-api/src/api/router.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
+
+export const router = Router()
+
+const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bookshelf API',
+      version: '0.1.0',
+      description: 'Documentation of Bookshelf API',
+    },
+  },
+  apis: ['src/api/routes/*.route.ts'],
+});
+
+
+router.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
--- bookshelf-api/src/main.ts
+++ bookshelf-api/src/main.ts
@@ -1,1 +1,18 @@
-console.log('Hello, World!')
+import cors from 'cors'
+import express from 'express'
+import { router } from './api/router'
+
+const app = express()
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000'
+  ],
+  credentials: true
+}))
+app.use(express.json())
+app.use(router)
+
+app.listen(1818, () => {
+  console.log('Server Running on http://localhost:1818')
+})
